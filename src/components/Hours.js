ed35771e2f239d6e440c3704ab76b5361eac4662
RubenMF99 <83036313+RubenMF99@users.noreply.github.com>
2024-08-27 22:38:48 +0000

    Se agregan Hours

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,24 @@
-// Placeholder to host a component
+import React from 'react';
+
+const Hours = () => {
+  const getOpeningHours = () => {
+    const today = new Date();
+    const dayOfWeek = today.getUTCDay();
+
+    if (dayOfWeek >= 1 && dayOfWeek <= 5) { 
+      return '10:00 AM - 4:00 PM';
+    } else { 
+      return '9:00 AM - 8:00 PM';
+    }
+  };
+
+  return (
+    <div>
+      <h2>Horas del dia</h2>
+      <p>{getOpeningHours()}</p>
+    </div>
+  );
+};
+
+export default Hours;
+
