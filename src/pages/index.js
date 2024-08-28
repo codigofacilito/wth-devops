de4a88846563e4a33eaf337ba086c30a74ee71e9
LinKJeT <45182407+LinKJeTT@users.noreply.github.com>
2024-08-28 16:16:29 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,14 @@ import dbConnect from "../lib/dbConnect";
 import Pet from "../models/Pet";
 
 // TODO: Import Hours component
-
+import Hours from '../components/Hours';
 const Index = ({ pets }) => {
 
   return (
     <>
 
       {/* TODO: Display Hours component */}
-
+      <Hours />
       {/* Create a card for each pet */}
       {pets.map((pet) => (
         <div key={pet._id}>
