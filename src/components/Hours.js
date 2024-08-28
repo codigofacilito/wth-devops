3df437892870aa5cfc32807527e4f54a2c2cd517
Bryan Vargas <105386759+briickhub@users.noreply.github.com>
2024-08-28 18:42:40 +0000

    Add Hours component to display shelter opening hours

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,26 +1,12 @@
 // components/Hours.js
 import React from 'react';
 
-// Función para obtener el horario de apertura basado en el día de la semana
-const getOpenHours = () => {
-  const today = new Date();
-  const dayOfWeek = today.getDay(); // 0 (Domingo) - 6 (Sábado)
-
-  if (dayOfWeek >= 1 && dayOfWeek <= 5) {
-    return '10 a.m. - 4 p.m.'; // Lunes a Viernes
-  } else {
-    return '9 a.m. - 8 p.m.'; // Sábado y Domingo
-  }
-};
-
 // Componente que muestra el horario de apertura
 const Hours = () => {
-  const hours = getOpenHours();
-
   return (
     <div>
       <h3>Horas de Apertura del Refugio</h3>
-      <p>Hoy estamos abiertos de: {hours}</p>
+      <p>El refugio abre de 10 a.m. a 4 p.m. de lunes a viernes, y de 9 a.m. a 8 p.m. los sábados y domingos.</p>
     </div>
   );
 };
